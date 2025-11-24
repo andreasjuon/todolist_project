606f0c61d601021fb5dc925fc9e844490ba6a14d
Andreas Juon <176597703+andreasjuon@users.noreply.github.com>
2025-11-24 17:33:46 +0100

    TodoList styling
    Styled the todolist, blue background, 100% vertical
    stretch, and making items appear with minimum width
    of 200px and then breaking the line.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Ugauga")
+console.log("Starting the todolist script, mate!")
+import "./styles.css";
+
+
+class Project {
+    constructor(title, description, color) {
+        this.title = title;
+        this.description = description;
+        this.color = color;
+    }
+
+}
+class ToDoItem {
+    constructor(title, description, project, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project;
+    }
+
+    //method to extend due date
+
+    //method to mark as in/complete
+}
